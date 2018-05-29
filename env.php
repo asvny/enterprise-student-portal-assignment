31b9eee19a4e85bd48234629cf8e1d678892560c
asvny <annamalai.saro@gmail.com>
2018-05-29 12:04:00 +1000

    Add files

---
+++ env.php
@@ -0,0 +1,14 @@
+<?php
+
+//  Database config
+define('DB_NAME', '****');
+define('DB_USER', '****');
+define('DB_PASSWORD', '****');
+
+// App Config
+define('APP_SALT','*****');
+
+define('ROLE_NORMAL', 0);
+define('ROLE_ADMINISTRATOR', 1);
+
+?>
